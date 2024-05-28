4bd3687f6a281959bc2350eddb87e8aac55dd577
Jhon Fredy Alzate <163074608+JhonFAlzate@users.noreply.github.com>
2024-05-28 17:29:07 -0500

    Creé la página de usuario se encuentra registrado, en login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,59 @@
 import { useForm } from "react-hook-form"
 import useAuth from "../hooks/useAuth"
 import '../components/styles/LoginPage.css'
+import { useEffect, useState } from "react"
 
 
 const LoginPage = () => {
 
      const {handleSubmit, register, reset}=useForm()
-
      const {loginUser} = useAuth()
-
+     
      const submit = data => {
-        
-        loginUser(data)
-
-        reset({
-            email: '',
-            password: ''
+       
+       loginUser(data)
+       
+       reset({
+         email: '',
+         password: ''
         })
-     }
+      }
+
+
+    /* Aquí estoy accediendo al local storage para capturar el usuario en un JSON*/
+    const [user, setUser] = useState({})
+     useEffect( () => {
+        const userFromLocalStorage = localStorage.getItem('userLogged')
+
+        if(userFromLocalStorage){
+
+          const parsedUser = JSON.parse(userFromLocalStorage)
+          setUser(parsedUser)
+        }
+
+     }, [])
+
+     /* Éso lo hice con ayuda del ChapGpt */
 
   return (
+    <section>
+    {
+
+      localStorage.getItem('token')
+
+     ?
+     (
+     <div className="login_user_registered">
+
+        <img className="login_user_registered_img" src="/male.png" alt="/male.png" />
+       
+        <h3 className="login_user_name"><span>Welcome,</span><span className="login_user_name_data">{user.firstName} {user.lastName}</span></h3>
+
+        <button className="login_user_btn">Logout</button>
+     </div> 
+     )
+
+    :
 
     <div className="login_container">
         <div className="login_img_title">
@@ -42,6 +76,10 @@ const LoginPage = () => {
         <button className="login_btn_submit">Submit</button>
       </form>
     </div>
+
+   
+    }
+    </section>
   )
 }
 
